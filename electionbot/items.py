c08a3cbc25b765813e431fd73029343ca4a12570
Eric Porter <eric.porter1234@gmail.com>
2019-09-24 21:41:22 -0400

    finished parties

--- electionbot/items.py
+++ electionbot/items.py
@@ -1,44 +1,61 @@
+import dateparser as dp
 import scrapy
-from scrapy.loader.processors import MapCompose, TakeFirst
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
 from w3lib.html import remove_tags
+
+
+def clean_field(value: str) -> str:
+    return value.replace(":", "")
+
+
+def resolve_relative_url(value: str) -> str:
+    return f"https://www.elections.ca{value}"
+
+
+def cleanse_address(value: str) -> str:
+    if (
+        value.strip().endswith(":")
+        or value.strip() == ""
+        or value.strip() == "Privacy Policy"
+    ):
+        return None
+    return value.replace("–", "")
+
+
+def cleanse_date(value: str) -> str:
+    try:
+        parsed = dp.parse(value)
+        if parsed is None:
+            return None
+        return str(parsed.date())
+    except Exception as e:
+        return None
 
 
 class Party(scrapy.Item):
     title = scrapy.Field(input_processor=MapCompose(str.strip))
-    short_name = scrapy.Field(input_processor=MapCompose(str.strip))
-    eligible_dt = scrapy.Field(input_processor=MapCompose(str.strip))
-    registered_dt = scrapy.Field(input_processor=MapCompose(str.strip))
-    deregistered_dt = scrapy.Field(input_processor=MapCompose(str.strip))
-    leader = scrapy.Field()
-    logo = scrapy.Field(
-        input_processor=MapCompose(lambda path: f"https://www.elections.ca{path}")
+    short_name = scrapy.Field(input_processor=MapCompose(clean_field, str.strip))
+    eligible_dt = scrapy.Field(
+        input_processor=MapCompose(clean_field, cleanse_date, str.strip)
     )
+    registered_dt = scrapy.Field(
+        input_processor=MapCompose(clean_field, cleanse_date, str.strip)
+    )
+    deregistered_dt = scrapy.Field(
+        input_processor=MapCompose(clean_field, cleanse_date, str.strip)
+    )
+    leader = scrapy.Field(input_processor=MapCompose(clean_field, str.strip))
+    logo = scrapy.Field(input_processor=MapCompose(resolve_relative_url))
     website = scrapy.Field()
-    national_hq_address_line_1 = scrapy.Field()
-    national_hq_address_line_2 = scrapy.Field()
-    national_hq_address_line_3 = scrapy.Field()
-    national_hq_address_line_4 = scrapy.Field()
-    national_hq_city = scrapy.Field()
-    national_hq_prov = scrapy.Field()
-    national_hq_postal_code = scrapy.Field()
-    national_hq_phone = scrapy.Field()
-    national_hq_fax = scrapy.Field()
-    chief_agent_address_line_1 = scrapy.Field()
-    chief_agent_address_line_2 = scrapy.Field()
-    chief_agent_address_line_3 = scrapy.Field()
-    chief_agent_address_line_4 = scrapy.Field()
-    chief_agent_address_line_5 = scrapy.Field()
-    chief_agent_city = scrapy.Field()
-    chief_agent_prov = scrapy.Field()
-    chief_agent_postal_code = scrapy.Field()
-    chief_agent_phone = scrapy.Field()
-    chief_agent_fax = scrapy.Field()
-    auditor_address_line_1 = scrapy.Field()
-    auditor_address_line_2 = scrapy.Field()
-    auditor_address_line_3 = scrapy.Field()
-    auditor_address_line_4 = scrapy.Field()
-    auditor_city = scrapy.Field()
-    auditor_prov = scrapy.Field()
-    auditor_postal_code = scrapy.Field()
-    auditor_phone = scrapy.Field()
-    auditor_fax = scrapy.Field()
+    national_headquarters = scrapy.Field(
+        input_processor=MapCompose(cleanse_address, clean_field, str.strip),
+        output_processor=Join("|"),
+    )
+    chief_agent = scrapy.Field(
+        input_processor=MapCompose(cleanse_address, clean_field, str.strip),
+        output_processor=Join("|"),
+    )
+    auditor = scrapy.Field(
+        input_processor=MapCompose(cleanse_address, clean_field, str.strip),
+        output_processor=Join("|"),
+    )
--- electionbot/spiders/parties.py
+++ electionbot/spiders/parties.py
@@ -16,14 +16,27 @@ class PartySpider(scrapy.Spider):
 
         for sel in response.xpath('//div[@class="borderbox1"]'):
             l = ItemLoader(item=ei.Party(), selector=sel)
-            l.add_xpath("title", "./h3/text()")
+            l.add_xpath("title", ".//h3//text()")
+            l.add_xpath("leader", ".//p[contains(span[1]/text(), 'Leader')]/text()")
             l.add_xpath("short_name", ".//p[contains(span[1]/text(), 'Short')]/text()")
-            l.add_xpath("eligible_dt", ".//p[contains(span[1]/text(), 'Eligible')]/text()")
-            l.add_xpath("registered_dt", ".//p[contains(span[1]/text(), 'Register')]/text()")
             l.add_xpath(
-                "deregistered_dt", ".//p[contains(span[1]/text(), 'Deregister')]/text()"
+                "eligible_dt", ".//p[contains(span[1]/text(), 'Eligible')]//text()"
+            )
+            l.add_xpath(
+                "registered_dt", ".//p[contains(span[1]/text(), 'Register')]//text()"
+            )
+            l.add_xpath(
+                "deregistered_dt", ".//p[contains(span[1]/text(), 'Deregister')]//text()"
             )
             l.add_xpath("website", "./h3/a/@href")
             l.add_xpath("logo", './/div[@class="logopp"]/img/@src')
+            l.add_xpath(
+                "national_headquarters",
+                ".//p[contains(span[1]/text(), 'Headquarter')]//text()",
+            )
+            l.add_xpath(
+                "chief_agent", ".//p[contains(span[1]/text(), 'Chief')]//text()"
+            )
+            l.add_xpath("auditor", ".//p[contains(span[1]/text(), 'Audit')]//text()")
             yield l.load_item()
 
